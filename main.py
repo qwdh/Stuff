1ccefee64bf66bbe7a0a71afe2b66eded5f12798
Jack Veg <140032678+qwdh@users.noreply.github.com>
2023-09-18 09:45:02 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,304 @@
+import math
+
+'''
+Project: Operation Calculator
+by: Jack aka RoboNuts
+Date made: 2/23/2022
+Date last updated: 2/28/2022
+
+Function List: +, -, *, /, sqrt, pow, abs
+'''
+
+print('Welcome to Jack\'s calculator!')
+numProblem = int(input('Please enter the amount of numbers in the problem: (1-10) '))
+
+if numProblem == 0:
+    print('haha nice try')
+
+if numProblem == 1:
+    print('What kind of function do you want to perform?')
+    userFunc = input('Function: ')
+
+    if userFunc == '+':
+        print('Needs another integer')
+
+    if userFunc == '-':
+        print('Needs another integer')
+
+    if userFunc == '*':
+        print('Needs another integer')
+
+    if userFunc == '/':
+        print('Needs another integer')
+
+    if userFunc == 'sqrt':
+        userNum = float(input('Enter an integer: '))
+        solvedFunc = math.sqrt(userNum)
+        print(f'{solvedFunc:.2f}')
+
+    if userFunc == 'pow':
+        userNum = float(input('Enter integer you want to raise:'))
+        userPow = int(input('Enter power: '))
+        solvedFunc = math.pow(userNum, userPow)
+        print(f'{solvedFunc:.2f}')
+
+    if userFunc == 'abs':
+        userNum = float(input('Enter your integer: '))
+        solvedFunc = math.fabs(userNum)
+        print(f'{solvedFunc:.2f}')
+
+if numProblem == 3:
+    print('What kind of function do you want to perform?')
+    userFunc = input('Function: ')
+
+    if userFunc == '+':
+        print('Needs another integer')
+
+    if userFunc == '-':
+        print('Needs another integer')
+
+    if userFunc == '*':
+        print('Needs another integer')
+
+    if userFunc == '/':
+        print('Needs another integer')
+
+    if userFunc == 'sqrt':
+        userNum = float(input('Enter an integer: '))
+        solvedFunc = math.sqrt(userNum)
+        print(f'{solvedFunc:.2f}')
+
+    if userFunc == 'pow':
+        userNum = float(input('Enter integer you want to raise:'))
+        userPow = int(input('Enter power: '))
+        solvedFunc = math.pow(userNum, userPow)
+        print(f'{solvedFunc:.2f}')
+
+    if userFunc == 'abs':
+        userNum = float(input('Enter your integer: '))
+        solvedFunc = math.fabs(userNum)
+        print(f'{solvedFunc:.2f}')
+
+if numProblem == 4:
+    print('What kind of function do you want to perform?')
+    userFunc = input('Function: ')
+
+    if userFunc == '+':
+        print('Needs another integer')
+
+    if userFunc == '-':
+        print('Needs another integer')
+
+    if userFunc == '*':
+        print('Needs another integer')
+
+    if userFunc == '/':
+        print('Needs another integer')
+
+    if userFunc == 'sqrt':
+        userNum = float(input('Enter an integer: '))
+        solvedFunc = math.sqrt(userNum)
+        print(f'{solvedFunc:.2f}')
+
+    if userFunc == 'pow':
+        userNum = float(input('Enter integer you want to raise:'))
+        userPow = int(input('Enter power: '))
+        solvedFunc = math.pow(userNum, userPow)
+        print(f'{solvedFunc:.2f}')
+
+    if userFunc == 'abs':
+        userNum = float(input('Enter your integer: '))
+        solvedFunc = math.fabs(userNum)
+        print(f'{solvedFunc:.2f}')
+
+if numProblem == 5:
+    print('What kind of function do you want to perform?')
+    userFunc = input('Function: ')
+
+    if userFunc == '+':
+        print('Needs another integer')
+
+    if userFunc == '-':
+        print('Needs another integer')
+
+    if userFunc == '*':
+        print('Needs another integer')
+
+    if userFunc == '/':
+        print('Needs another integer')
+
+    if userFunc == 'sqrt':
+        userNum = float(input('Enter an integer: '))
+        solvedFunc = math.sqrt(userNum)
+        print(f'{solvedFunc:.2f}')
+
+    if userFunc == 'pow':
+        userNum = float(input('Enter integer you want to raise:'))
+        userPow = int(input('Enter power: '))
+        solvedFunc = math.pow(userNum, userPow)
+        print(f'{solvedFunc:.2f}')
+
+    if userFunc == 'abs':
+        userNum = float(input('Enter your integer: '))
+        solvedFunc = math.fabs(userNum)
+        print(f'{solvedFunc:.2f}')
+
+if numProblem == 6:
+    print('What kind of function do you want to perform?')
+    userFunc = input('Function: ')
+
+    if userFunc == '+':
+        print('Needs another integer')
+
+    if userFunc == '-':
+        print('Needs another integer')
+
+    if userFunc == '*':
+        print('Needs another integer')
+
+    if userFunc == '/':
+        print('Needs another integer')
+
+    if userFunc == 'sqrt':
+        userNum = float(input('Enter an integer: '))
+        solvedFunc = math.sqrt(userNum)
+        print(f'{solvedFunc:.2f}')
+
+    if userFunc == 'pow':
+        userNum = float(input('Enter integer you want to raise:'))
+        userPow = int(input('Enter power: '))
+        solvedFunc = math.pow(userNum, userPow)
+        print(f'{solvedFunc:.2f}')
+
+    if userFunc == 'abs':
+        userNum = float(input('Enter your integer: '))
+        solvedFunc = math.fabs(userNum)
+        print(f'{solvedFunc:.2f}')
+
+if numProblem == 7:
+    print('What kind of function do you want to perform?')
+    userFunc = input('Function: ')
+
+    if userFunc == '+':
+        print('Needs another integer')
+
+    if userFunc == '-':
+        print('Needs another integer')
+
+    if userFunc == '*':
+        print('Needs another integer')
+
+    if userFunc == '/':
+        print('Needs another integer')
+
+    if userFunc == 'sqrt':
+        userNum = float(input('Enter an integer: '))
+        solvedFunc = math.sqrt(userNum)
+        print(f'{solvedFunc:.2f}')
+
+    if userFunc == 'pow':
+        userNum = float(input('Enter integer you want to raise:'))
+        userPow = int(input('Enter power: '))
+        solvedFunc = math.pow(userNum, userPow)
+        print(f'{solvedFunc:.2f}')
+
+    if userFunc == 'abs':
+        userNum = float(input('Enter your integer: '))
+        solvedFunc = math.fabs(userNum)
+        print(f'{solvedFunc:.2f}')
+
+if numProblem == 8:
+    print('What kind of function do you want to perform?')
+    userFunc = input('Function: ')
+
+    if userFunc == '+':
+        print('Needs another integer')
+
+    if userFunc == '-':
+        print('Needs another integer')
+
+    if userFunc == '*':
+        print('Needs another integer')
+
+    if userFunc == '/':
+        print('Needs another integer')
+
+    if userFunc == 'sqrt':
+        userNum = float(input('Enter an integer: '))
+        solvedFunc = math.sqrt(userNum)
+        print(f'{solvedFunc:.2f}')
+
+    if userFunc == 'pow':
+        userNum = float(input('Enter integer you want to raise:'))
+        userPow = int(input('Enter power: '))
+        solvedFunc = math.pow(userNum, userPow)
+        print(f'{solvedFunc:.2f}')
+
+    if userFunc == 'abs':
+        userNum = float(input('Enter your integer: '))
+        solvedFunc = math.fabs(userNum)
+        print(f'{solvedFunc:.2f}')
+
+if numProblem == 9:
+    print('What kind of function do you want to perform?')
+    userFunc = input('Function: ')
+
+    if userFunc == '+':
+        print('Needs another integer')
+
+    if userFunc == '-':
+        print('Needs another integer')
+
+    if userFunc == '*':
+        print('Needs another integer')
+
+    if userFunc == '/':
+        print('Needs another integer')
+
+    if userFunc == 'sqrt':
+        userNum = float(input('Enter an integer: '))
+        solvedFunc = math.sqrt(userNum)
+        print(f'{solvedFunc:.2f}')
+
+    if userFunc == 'pow':
+        userNum = float(input('Enter integer you want to raise:'))
+        userPow = int(input('Enter power: '))
+        solvedFunc = math.pow(userNum, userPow)
+        print(f'{solvedFunc:.2f}')
+
+    if userFunc == 'abs':
+        userNum = float(input('Enter your integer: '))
+        solvedFunc = math.fabs(userNum)
+        print(f'{solvedFunc:.2f}')
+
+if numProblem == 10:
+    print('What kind of function do you want to perform?')
+    userFunc = input('Function: ')
+
+    if userFunc == '+':
+        print('Needs another integer')
+
+    if userFunc == '-':
+        print('Needs another integer')
+
+    if userFunc == '*':
+        print('Needs another integer')
+
+    if userFunc == '/':
+        print('Needs another integer')
+
+    if userFunc == 'sqrt':
+        userNum = float(input('Enter an integer: '))
+        solvedFunc = math.sqrt(userNum)
+        print(f'{solvedFunc:.2f}')
+
+    if userFunc == 'pow':
+        userNum = float(input('Enter integer you want to raise:'))
+        userPow = int(input('Enter power: '))
+        solvedFunc = math.pow(userNum, userPow)
+        print(f'{solvedFunc:.2f}')
+
+    if userFunc == 'abs':
+        userNum = float(input('Enter your integer: '))
+        solvedFunc = math.fabs(userNum)
+        print(f'{solvedFunc:.2f}')
